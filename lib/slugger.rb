3fabc73e30cc5a4dcfe248238c1ec78234f0752e
Seth Faxon <seth.faxon@gmail.com>
2011-08-23 17:29:40 -0700

    fix typo

--- lib/slugger.rb
+++ lib/slugger.rb
@@ -7,35 +7,35 @@ module Slugger
     base.extend(ClassMethods)
   end
   module ClassMethods
-    def has_slug(title_column=nil,sluger_options={})
-      class_inheritable_accessor :sluger_options
-      sluger_options[:title_column]      ||= title_column || 'title'
-      # sluger_options[:title_column]      ||= 'title'
-      sluger_options[:slug_column]       ||= 'slug'
-      sluger_options[:as_param]          ||= true
-      sluger_options[:substitution_char] ||= '-'
-      self.sluger_options = sluger_options
+    def has_slug(title_column=nil,slugger_options={})
+      class_inheritable_accessor :slugger_options
+      slugger_options[:title_column]      ||= title_column || 'title'
+      # slugger_options[:title_column]      ||= 'title'
+      slugger_options[:slug_column]       ||= 'slug'
+      slugger_options[:as_param]          ||= true
+      slugger_options[:substitution_char] ||= '-'
+      self.slugger_options = slugger_options
       
-      # if columns_hash[sluger_options[:title_column].to_s].nil?
+      # if columns_hash[slugger_options[:title_column].to_s].nil?
       # 
       #   raise ArgumentError, "#{self.name} is missing source column"
       # end
-      raise ArgumentError, "#{self.name} is missing required slug column" if columns_hash[sluger_options[:slug_column]].nil?
+      raise ArgumentError, "#{self.name} is missing required slug column" if columns_hash[slugger_options[:slug_column]].nil?
 
       before_validation :create_slug, :on => :create
 
-      validates sluger_options[:slug_column].to_sym, :presence => true
-      if sluger_options[:scope]
-        validates sluger_options[:slug_column].to_sym, :uniqueness => {:scope => sluger_options[:scope]}
+      validates slugger_options[:slug_column].to_sym, :presence => true
+      if slugger_options[:scope]
+        validates slugger_options[:slug_column].to_sym, :uniqueness => {:scope => slugger_options[:scope]}
       else
-        validates sluger_options[:slug_column].to_sym, :uniqueness => true
+        validates slugger_options[:slug_column].to_sym, :uniqueness => true
       end
 
-      send :define_method, :column_to_slug, lambda { self.send(sluger_options[:title_column]) }
+      send :define_method, :column_to_slug, lambda { self.send(slugger_options[:title_column]) }
 
       class << self
         def find(*args)
-          if self.sluger_options[:as_param] && args.first.is_a?(String)
+          if self.slugger_options[:as_param] && args.first.is_a?(String)
             find_by_slug(args)
           else
             super(*args)
@@ -49,7 +49,7 @@ module Slugger
   module InstanceMethods
 
     def to_param
-      sluger_options[:as_param] ? self.slug : self.id
+      slugger_options[:as_param] ? self.slug : self.id
     end
 
     protected
@@ -73,7 +73,7 @@ module Slugger
     end
   
     def clean(string)
-      string.downcase.gsub(/[^\w\s\d\_\-]/,'').gsub(/\s\s+/,' ').gsub(/[^\w\d]/, sluger_options[:substitution_char])
+      string.downcase.gsub(/[^\w\s\d\_\-]/,'').gsub(/\s\s+/,' ').gsub(/[^\w\d]/, slugger_options[:substitution_char])
     end
   end
 end
