d6e3a28b841e8da1dd003c807d672c62ca20d0aa
Jordan Byron <jordan@ducksoupsoftware.com>
2012-01-06 22:51:43 -0500

    Add append_id conflict resolution

--- spec/lib/user_spec.rb
+++ spec/lib/user_spec.rb
@@ -9,4 +9,11 @@ describe User do
     u = User.create(:first_name => "Tyler", :last_name => "Durden")
     u.slug.should == "tyler-durden"
   end
+  it "should append the database id if duplicates exist" do
+    first_user = User.create(:first_name => "Jordan", :last_name => "Byron")
+    first_user.slug.should == "jordan-byron"
+
+    second_user = User.create(:first_name => "Jordan", :last_name => "Byron")
+    second_user.slug.should == "jordan-byron-#{second_user.id}"
+  end
 end
